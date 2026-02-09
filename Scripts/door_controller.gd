986cb378f089ecdb7ae34f1dee04b0e041274a87
Jonathan Vasquez <vasquez_jonathan@wheatoncollege.edu>
2026-02-09 10:21:52 -0500

    Player goes through door without teleporting back and forth but 2nd floor collider is broken

--- Scripts/door_controller.gd
+++ Scripts/door_controller.gd
@@ -6,5 +6,7 @@ extends Node2D
 func teleport_body(body: Node2D) -> void:
 	if body.position.y > 130 :
 		body.position = door2.position
+		body.position.x = door2.position.x + 200
 	else:
 		body.position = door1.position
+		body.position.x = door1.position.x - 200
