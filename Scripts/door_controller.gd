652ad501194a182f82b01328f5bb573047e3db21
Jonathan Vasquez <vasquez_jonathan@wheatoncollege.edu>
2026-02-11 09:45:33 -0500

    Player teleports through door with proper floor collision/ BUG: Player can't teleport back from 2nd door

--- Scripts/door_controller.gd
+++ Scripts/door_controller.gd
@@ -3,10 +3,18 @@ extends Node2D
 @onready var door1 = $Door
 @onready var door2 = $Door2
 
+var timer: float = 0.0
+
+func _physics_process(delta:float) -> void:
+	if timer > 0.0:
+		timer -= 1 * delta
+
 func teleport_body(body: Node2D) -> void:
-	if body.position.y > 130 :
-		body.position = door2.position
-		body.position.x = door2.position.x + 200
-	else:
-		body.position = door1.position
-		body.position.x = door1.position.x - 200
+	if timer <= 0.0:
+		if body.position.y > 130 :
+			body.position = door2.position
+			body.position.x = door2.position.x
+		else:
+			body.position = door1.position
+			body.position.x = door1.position.x
+		timer = 1.0
